fff745aa80ab97550636c40bca6d6b7af041258e
richard <richardsunday0812@gmail.com>
2026-03-12 12:39:36 +0100

    Refactor navigation links to use NavLink for active styling

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,16 +1,26 @@
-import { Link } from "react-router-dom";
-import styles from "../MainNavigation.module.css"; 
+import { NavLink } from "react-router-dom";
+import styles from "../MainNavigation.module.css";
 
 export function MainNavigation() {
   return (
     <header className={styles.header}>
-      <nav >
+      <nav>
         <ul className={styles.navigation}>
           <li className={styles.list}>
-            <Link to="/">Home</Link>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+            >
+              Home
+            </NavLink>
           </li>
           <li className={styles.list}>
-            <Link to="/products">Products</Link>
+            <NavLink
+              to="/products"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
