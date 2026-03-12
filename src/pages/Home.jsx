451a6dd99154b2170281d792daf83a16d24c25d7
richard <richardsunday0812@gmail.com>
2026-03-12 12:45:30 +0100

    Replace Link with button for navigation to Products Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate("/products");
+  }
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
       <p>This is the main landing page of our application.</p>
-      <p>Go to the <Link to="/products">Products Page</Link>.</p>
+      <p>
+        <button onClick={navigateHandler}>Go to Products Page</button>
+      </p>
     </div>
   );
 }
